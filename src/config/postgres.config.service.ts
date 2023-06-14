38308550bfd7f40521cac32304d3a645688936c8
Abner André <abnerndr.dev@gmail.com>
2023-06-14 17:59:40 -0300

    fist commit

---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [User],
+      synchronize: true,
+    };
+  }
+}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository {
+  private user = [];
+
+  async save(user) {
+    this.user.push(user);
+  }
+
+  async list() {
+    return this.user;
+  }
+}
